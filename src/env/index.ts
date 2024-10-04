4fd680c7de67345f56a25814d1de6f7b68f31af1
Rntzin <kinglemoyra@gmail.com>
2024-10-04 12:02:00 -0300

    feat: testes unitários

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "text", "production"]).default("dev"),
+  NODE_ENV: z.enum(["dev", "text", "production", "test"]).default("dev"), // Adicionei "test"
   PORT: z.coerce.number().default(3333),
 });
 
